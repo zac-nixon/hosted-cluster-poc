c8646c66e432d65c3da8918fff9692dc010d0484
Zac Nixon <zac.nixon@ibm.com>
2019-10-28 15:55:41 -0500

    fix render.sh

--- openvpn/render.sh
+++ openvpn/render.sh
@@ -20,3 +20,4 @@ envsubst < openvpn-spec.yaml > ../manifests/managed/openvpn-spec.yaml
 envsubst < openvpn-clusterrole.yaml > ../manifests/managed/openvpn-clusterrole.yaml
 envsubst < openvpn-rb.yaml > ../manifests/managed/openvpn-rb.yaml
 envsubst < openvpn-sa.yaml > ../manifests/managed/openvpn-sa.yaml
+envsubst < openvpn-operator.yaml > ../manifests/managed/openvpn-operator.yaml
